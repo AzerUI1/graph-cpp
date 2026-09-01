8c0903a1ec6d0c0ba60c6fba1f95aa10ad84a31b
Azar Aslanov <printhelloprogramming@gmail.com>
2026-09-02 00:50:30 +0400

    Implement advanced DFS with timing and color tracking
    Added advanced version of DFS with timing and color tracking.

--- DFS.cpp
+++ DFS.cpp
@@ -1,3 +1,4 @@
+// Easy Version
 vector<vector<int>> adj_list;
 int n;
 
@@ -9,4 +10,23 @@ void dfs(int v) {
         if (!visited[u])
             dfs(u);
     }
+}
+
+// Advanced Version
+vector<vector<int>> adj;
+int n;
+
+vector<int> color;
+
+vector<int> time_in, time_out;
+int dfs_timer = 0;
+
+void dfs(int v) {
+    time_in[v] = dfs_timer++;
+    color[v] = 1;
+    for (int u : adj[v])
+        if (color[u] == 0)
+            dfs(u);
+    color[v] = 2;
+    time_out[v] = dfs_timer++;
 }
